a0044d35ab6510637f93d27e234e2e806a07cc65
Daniel Hartley <daniel.hartley@altruistiq.com>
2025-04-21 08:55:43 +0100

    refactor: simplify component selection based on types

--- src/components/test/TestDisplay.tsx
+++ src/components/test/TestDisplay.tsx
@@ -1,9 +1,4 @@
-import {
-  Layout,
-  ContentHandlerType,
-  QuestionType,
-  DistractorType,
-} from '@/types'
+import { Layout, QuestionType } from '@/types'
 import ImageChoiceComponent from '@/components/test/layouts/ImageChoiceComponent'
 import MultipleTextChoiceComponent from '@/components/test/layouts/MultipleTextChoiceComponent'
 import TextEntryComponent from '@/components/test/layouts/TextEntryComponent'
@@ -13,41 +8,39 @@ type Props<T> = {
   onSubmit: (answer: string) => void
 }
 
-type DisplayKey = `${ContentHandlerType}+${DistractorType}+${QuestionType}`
+// Create a more robust mapping system with proper TypeScript support
+type ComponentMap = {
+  [key in QuestionType]: {
+    default: React.ComponentType<any>
+    image?: React.ComponentType<any>
+    // Add other specific overrides as needed
+  }
+}
 
-const displayTypeMap: Record<DisplayKey, React.ComponentType<any>> = {
-  // Taxonomy mappings
-  'taxonomy+image+Multiple choice': ImageChoiceComponent,
-  'taxonomy+vernacularName+Multiple choice': MultipleTextChoiceComponent,
-  'taxonomy+binomial+Multiple choice': MultipleTextChoiceComponent,
-  'taxonomy+genus+Multiple choice': MultipleTextChoiceComponent,
-  'taxonomy+species+Multiple choice': MultipleTextChoiceComponent,
-  'taxonomy+image+Text entry': TextEntryComponent,
-  'taxonomy+vernacularName+Text entry': TextEntryComponent,
-  'taxonomy+binomial+Text entry': TextEntryComponent,
-  'taxonomy+genus+Text entry': TextEntryComponent,
-  'taxonomy+species+Text entry': TextEntryComponent,
-
-  // Definition mappings
-  'definition+image+Multiple choice': ImageChoiceComponent,
-  'definition+vernacularName+Multiple choice': MultipleTextChoiceComponent,
-  'definition+binomial+Multiple choice': MultipleTextChoiceComponent,
-  'definition+genus+Multiple choice': MultipleTextChoiceComponent,
-  'definition+species+Multiple choice': MultipleTextChoiceComponent,
-  'definition+image+Text entry': TextEntryComponent,
-  'definition+vernacularName+Text entry': TextEntryComponent,
-  'definition+binomial+Text entry': TextEntryComponent,
-  'definition+genus+Text entry': TextEntryComponent,
-  'definition+species+Text entry': TextEntryComponent,
+const displayComponentMap: ComponentMap = {
+  'Multiple choice': {
+    default: MultipleTextChoiceComponent,
+    image: ImageChoiceComponent,
+  },
+  'Text entry': {
+    default: TextEntryComponent,
+    // Text entry uses the same component regardless of distractor type
+  },
 }
 
 export function TestDisplay<T>({ layout, onSubmit }: Props<T>) {
   const distractorType = layout.distractorType ?? 'binomial'
-  const contentType = layout.collection.type
   const questionType = layout.question.type
-  const displayType =
-    `${contentType}+${distractorType}+${questionType}` as DisplayKey
-  console.log(displayType)
-  const Component = displayTypeMap[displayType]
+
+  // Get the map for this question type
+  const questionMap = displayComponentMap[questionType]
+
+  // Check if there's a specific component for this distractor type
+  // If not, fall back to the default
+  const Component =
+    distractorType === 'image' && questionMap.image
+      ? questionMap.image
+      : questionMap.default
+
   return <Component question={layout.question} onSubmit={onSubmit} />
 }
