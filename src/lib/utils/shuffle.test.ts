25c03bec922cd508964e08e4680ffc2b80bfb079
Daniel Hartley <daniel.hartley@altruistiq.com>
2025-05-09 14:29:22 +0100

    refactor: remove old suffle file and tests, unused distractors

--- src/lib/utils/shuffle.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, it, expect } from 'vitest'
-import { shuffle } from '@/utils/shuffle'
-
-describe('shuffle', () => {
-  it('should return [] for empty array', () => {
-    expect(shuffle([])).toEqual([])
-  })
-})
