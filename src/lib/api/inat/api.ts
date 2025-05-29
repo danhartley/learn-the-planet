91f82c7c78f46e5f26497d6eecd2845779fd0e27
Daniel Hartley <daniel.hartley@altruistiq.com>
2025-05-29 08:58:19 +0100

    fix: fix the inat look up for trait example taxa

--- src/lib/api/inat/api.ts
+++ src/lib/api/inat/api.ts
@@ -1,5 +1,5 @@
 import { mapInatSpeciesToLTP } from '@/api/inat/inat-species-map'
-import { ContentHandlerType, LearningItem } from '@/types'
+import { ContentHandlerType, LearningItem, Taxon, Trait } from '@/types'
 
 async function getInatData(url: string) {
   try {
@@ -24,16 +24,38 @@ type Props = {
 export const getInatTaxonProperties = async ({
   items,
   type,
-}: Props): Promise<LearningItem[] | undefined> => {
+}: Props): Promise<unknown[] | undefined> => {
+  let ids: string[], url: string, inatProperties: Taxon[] | undefined
   switch (type) {
     case 'taxon':
-      const ids = items.map(item => item.id)
-      const url = `https://api.inaturalist.org/v1/taxa/${ids.join(',')}`
-      const inatProperties = await getInatData(url)
-      return items.map((item: LearningItem) => {
+      ids = items.map(item => item.id)
+      url = `https://api.inaturalist.org/v1/taxa/${ids.join(',')}`
+      inatProperties = await getInatData(url)
+      if (!inatProperties) {
+        return items
+      }
+      return (items as Taxon[]).map((item: Taxon) => {
         return {
           ...inatProperties?.find(prop => prop.id === item.id),
           ...item,
+        }
+      })
+    case 'trait':
+      ids = (items as Trait[]).flatMap(i => i.examples?.map(e => e.id) ?? [])
+      url = `https://api.inaturalist.org/v1/taxa/${ids.join(',')}`
+      inatProperties = await getInatData(url)
+      if (!inatProperties) {
+        return items
+      }
+      return (items as Trait[]).map((item: Trait) => {
+        return {
+          ...item,
+          examples: item.examples?.map((example: Taxon) => {
+            return {
+              ...example,
+              ...inatProperties?.find(prop => prop.id === example.id),
+            }
+          }),
         }
       })
     default:
