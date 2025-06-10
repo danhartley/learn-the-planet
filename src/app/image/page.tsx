c479f6546ad765251247fe07b1d14ab21c1f4b26
Daniel Hartley <daniel.hartley@altruistiq.com>
2025-06-10 15:36:33 +0100

    refactor: pass props to cloudinary image

--- src/app/image/page.tsx
+++ src/app/image/page.tsx
@@ -1,6 +1,45 @@
 'use client'
+import { useState } from 'react'
+
+import { CloudinaryUploadWidgetOptions } from 'next-cloudinary'
+
 import { ImageUpload } from '@/components/image/ImageUpload'
 
 export default function Page() {
-  return <ImageUpload />
+  const [title, setTitle] = useState('')
+  const [description, setDescription] = useState('')
+
+  const options: CloudinaryUploadWidgetOptions = {
+    maxFileSize: 1000000, // 1MB in bytes
+    context: {
+      title,
+      description,
+    },
+  }
+  return (
+    <>
+      <h1>Upload a new image</h1>
+      <form action="">
+        <label htmlFor="image-title">Title</label>
+        <input
+          id="image-title"
+          type="text"
+          value={title}
+          placeholder="Enter image title"
+          title="Image title input field"
+          onChange={e => setTitle(e.target.value)}
+        />
+        <label htmlFor="image-description">Description</label>
+        <input
+          id="image-description"
+          type="text"
+          value={description}
+          placeholder="Enter image description"
+          title="Image description input field"
+          onChange={e => setDescription(e.target.value)}
+        />
+      </form>
+      <ImageUpload options={options} />
+    </>
+  )
 }
