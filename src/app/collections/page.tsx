43880811cf35a6401a78aaa7ba7246dfd4c1a16f
Daniel Hartley <daniel.hartley@altruistiq.com>
2025-06-13 10:01:18 +0100

    refactor: test removing caching for collection summaries

--- src/app/collections/page.tsx
+++ src/app/collections/page.tsx
@@ -1,3 +1,6 @@
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
+
 import { CollectionList } from '@/components/CollectionList'
 import { CollectionSummary } from '@/types'
 import { getCollectionSummaries } from '@/api/database'
